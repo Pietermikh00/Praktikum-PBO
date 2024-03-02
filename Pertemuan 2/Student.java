855d9ad91e231b413d3e3aa95887acff7f18115f
Alcendol <117699108+Alcendol@users.noreply.github.com>
2024-03-02 22:19:41 +0700

    Add files via upload

---
+++ Pertemuan 2/Student.java
@@ -0,0 +1,89 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Student {
+    /* TODO: implementasi enkapsulasi dengan 
+       berikan akses yang sesuai
+    */
+    private String name;
+    private int age;
+    private String address;
+    private int studentID;
+    private List<Course> coursesEnrolled;
+
+    public Student(String name, int age, String address, int studentID) {
+        // TODO: buatlah fungsi konstruktor
+        this.name=name;
+        this.age=age;
+        this.address=address;
+        this.studentID=studentID;
+        this.coursesEnrolled = new ArrayList<>();
+    }
+
+    public void getDetails() {
+        /* TODO: buat fungsi untuk print detail dari Student,
+           menampilkan nama, umur, alamat, dan ID
+        */
+       System.out.println("Nama: "+this.getStudentName());
+       System.out.println("Umur: "+this.getStudentAge());
+       System.out.println("Alamat: "+this.getStudentAddress());
+       System.out.println("NIM: "+this.getStudentID()+"\n");
+    }
+
+    public void enrollInCourse(Course course) {
+        // TODO: buatlah fungsi untuk menambah sebuah course
+        this.coursesEnrolled.add(course);
+    }
+
+    public void viewEnrolledCourses() {
+        /* TODO: buatlah fungsi untuk menampilkan seluruh course 
+           yang diambil oleh mahasiswa.
+
+           Hint: gunakan loop dan method getDetails dari Course
+        */
+        for (Course course : coursesEnrolled){
+            course.getDetails();
+        }
+    }
+
+    // Other methods...
+    public String getStudentName(){
+        return this.name;
+    }
+
+    public int getStudentAge(){
+        return this.age;
+    }
+
+    public String getStudentAddress(){
+        return this.address;
+    }
+
+    public int getStudentID(){
+        return this.studentID;
+    }
+
+    public void setStudentName(String name){
+        this.name = name;
+    }
+
+    public void setStudentAge(int age){
+        this.age = age;
+    }
+
+    public void setStudentAddress(String address){
+        this.address = address;
+    }
+
+    public void setStudentID(int studentID){
+        this.studentID = studentID;
+    }
+
+    public void removeCourse(Course course){
+        /* Menghapus course dari coursesEnrolled */
+        if(coursesEnrolled.contains(course)){
+            this.coursesEnrolled.remove(course);
+        }
+    }
+
+}
