44ded21a115a5a2583975fb5a40b7513da4c888f
Aldisar Gibran <117699108+Alcendol@users.noreply.github.com>
2024-05-28 23:35:18 +0700

    Add files via upload

---
+++ Airplane.java
@@ -0,0 +1,10 @@
+/**
+ * File: Airplane.java
+ * Deskripsi: abstract class Airplane
+ * Nama/NIM : Aldisar Gibran/24060122130081
+ * Tanggal  : 28 Mei 2024
+ */
+
+public abstract class Airplane extends Vehicle implements IFlyer{
+    
+}
